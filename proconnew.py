c2d5b3ef8c2fe8d02d3594e96ce000358f72c22b
root <root@vm1.(none)>
2014-04-13 00:42:40 +0530

    proconnew updated

--- proconnew.py
+++ proconnew.py
@@ -1,29 +1,29 @@
 from multiprocessing import Process, Lock, Queue
 import random, hashlib, time, os
-import numpy
+import numpy,time
 from numpy import random
-
+from apscheduler.scheduler import Scheduler
 def worker(queue,master_lock):
-    while(1):
+    #while(1):
         #print 'Got into consumer method, with pid: %s' % os.getpid()
-         master_lock.acquire()
+         #master_lock.acquire()
          if queue.qsize() != 0:
             A = queue.get()
+            #master_lock.release()
             #A=numpy.dot(A,A)
             print 'got msg: \n', A
          else:
             A = None
             print 'Queue looks empty'
-         master_lock.release()
          #time.sleep(random.randrange(5,10))
-def master(queue,worker_lock):
-    while(1):
+def master(queue):
+    #while(1):
        #print 'Got into producer method, with pid: %s' % os.getpid()
-       worker_lock.acquire()
-       A=random.rand(2,2)       
+       #worker_lock.acquire()
+       A=random.rand(2000,2000)       
        queue.put(A)
        print 'Produced msg: \n', A
-       worker_lock.release()
+       #worker_lock.release()
        #time.sleep(random.randrange(5,10))
 
 if __name__ == "__main__": 
@@ -31,9 +31,18 @@ if __name__ == "__main__":
     master_lock = Lock()
     worker_lock = Lock()
     #l=len(os.listdir("/home/sarath/Desktop"))
-    l=2
-    master(queue,master_lock)
-    worker(queue,worker_lock)
+    sched=Scheduler()
+    sched.start()
+    sched.add_cron_job(lambda: master(queue), second='*/1')
+    while True:
+        time.sleep(1)
+        if queue.qsize()!=0:
+            for j in range(queue.qsize()):
+                Process(target=worker, args=(queue,master_lock)).start()
+        else:
+            break
+        #worker(queue,worker_lock)    
+        #master(queue,master_lock)
 '''    while(1):
         if l !=0:
             for i in range(l):
