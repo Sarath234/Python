4526edd775438bc84bf2aa7ba4388bc24a313c03
root <root@vm1.(none)>
2014-04-16 22:50:14 +0530

    update

--- queueeee.py
+++ queueeee.py
@@ -1,19 +1,30 @@
 def worker(input_q):
     while True:
-        item=input_q.get()
-        print item
+        input_q.get()
+        
+        subprocess.call('mpiexec -n 1 python mulpar1.py &',shell=True)
+        print 'I am Worker'
         input_q.task_done()
 
 def master(sequence,output_q):
     for item in sequence:
-        output_q.put(item)
-
+        subprocess.call('python creatmat.py',shell=True)
+        output_q.put(1)
+    time.sleep(1)
 if __name__== "__main__":
     from multiprocessing import Process, JoinableQueue
+    from numpy import *
+    import subprocess
+    import math
+    import pwd
+    import grp
+    import os,time
+    import numpy as np
+
     q=JoinableQueue()
     con_p=Process(target=worker,args=(q,))
     con_p.daemon=True
     con_p.start()
-    sequence=range(100)
+    sequence=range(4)
     master(sequence,q)
     q.join()
