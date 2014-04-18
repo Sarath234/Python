0b51a3f333e85d8a38b1d619194f11d06e886c40
Sarath <m.sarath234@gmail.com>
2014-04-19 02:10:48 +0530

    Random Matrix Size

--- mulpar.py
+++ mulpar.py
@@ -8,16 +8,17 @@ t=time.time() #to get computation time
 comm=MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
-B=sys.argv[1]
-A=loadtxt(B,delimiter=',') # A Matrix 
+A=loadtxt(sys.argv[1],delimiter=',') # A Matrix 
 #print A
 l=len(A) #No of Rows
 n=len(A)/size #No of rows for Each processor
 rem=len(A)%size #Remaining rows
+
 if rem==0:
     a=rank*n
     b=a+n
     C=dot(A[a:b,:],A)
+
 else:
     if rank!=size-1:
         a=rank*n
@@ -28,7 +29,7 @@ else:
         D=dot(A[a:len(A),:],A)
 
 if rank==0:
-    subprocess.call('rm '+B,shell=True)
+    subprocess.call('rm '+sys.argv[1],shell=True)
     E=zeros_like(A)
     E[a:b,:]=C
     if rem==0:
