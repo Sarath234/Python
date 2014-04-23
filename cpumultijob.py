960a5bc5e906c832ce894c55acd769c6e58f759d
Sarath <m.sarath234@gmail.com>
2014-04-23 17:53:31 +0530

    cpumultijob.py iz backup for proconnew.py

---
+++ cpumultijob.py
@@ -0,0 +1,45 @@
+from multiprocessing import Process, Lock, Queue
+from numpy import *
+import subprocess
+import math
+import pwd
+import grp
+import os,time
+import numpy as np
+
+def worker(queue,queue1):
+    while True:
+        print 'before worker',queue1.qsize()
+        if queue1.qsize()!=0:
+            for j in range(queue1.qsize()):
+                B=queue.get()
+                queue1.get()
+                subprocess.call('mpiexec -f hosts -n 2 python mulpar1.py '+B+'&', shell=True)                
+        else:
+            print 'Queue is Empty'
+        time.sleep(5)
+
+def master(queue,queue1):
+    j=0
+    while True:
+        uid=pwd.getpwnam('nobody').pw_uid
+        gid=grp.getgrnam('nogroup').gr_gid
+        matrix_size=[2000,2500]
+        l=matrix_size[random.randint(1)]
+        A=random.rand(l,l).astype('d')
+        savetxt('mat'+str(j)+'.txt',A,delimiter=',',fmt='%3.3f')
+        A=None
+        os.chown('mat'+str(j)+'.txt',uid,gid)
+        queue.put('mat'+str(j)+'.txt')
+        queue1.put(1)
+        j=j+1
+        per=(subprocess.check_output('ssh root@192.168.32.218 nohup python /export/user/psutilexe.py',stdin=None,stderr=subprocess.STDOUT,shell=True)).split(' ')
+        print 'CPU %=',float(per[0])
+        print 'MEM %=',float(per[1])
+        time.sleep(2)
+       
+if __name__ == "__main__": 
+    queue = Queue()
+    queue1 = Queue()
+    Process(target=master,args=(queue,queue1,)).start()
+    Process(target=worker, args=(queue,queue1,)).start()
