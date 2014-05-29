a87f163204120b2ab2482fabb3971da99d68f3d2
root <root@vm1.(none)>
2014-05-29 22:49:37 +0530

    SSH_FREEVMS

--- normalmul.py
+++ normalmul.py
@@ -3,6 +3,7 @@ import math
 import pickle
 import time
 t=time.time()
-A=pickle.load(open('mat','rb'))
+#A=pickle.load(open('mat','rb'))
+A=loadtxt('mat.txt',delimiter=',')
 print dot(A,A)
 print time.time()-t
