cb75f896e4879684a032f12f1be0880f106b0184
Sarath234 <m.sarath234@gmail.com>
2014-09-05 10:16:21 +0530

    Delete client_test.py

--- client_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import socket
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('localhost',8081))
-s.send('Hi...!')
-data=s.recv(1024)
-s.close()
-print 'Recieved: '
-print data
