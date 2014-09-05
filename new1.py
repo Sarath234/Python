77b2a7f04134d8c8bcffaf9187e299136d39cd37
Sarath234 <m.sarath234@gmail.com>
2014-09-05 10:21:02 +0530

    Delete new1.py

--- new1.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import time
-t=time.time()
-per=(subprocess.check_output('ssh root@192.168.32.218 nohup python /export/user/psutilexe.py',stdin=None,stderr=subprocess.STDOUT,shell=True)).split(' ')
-print 'CPU %=',float(per[0])
-print 'MEM %=',float(per[1])
-print time.time()-t
